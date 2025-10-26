83aed56152ed7c9dffa911e771bbb36a97e1c4af
Adil El Bahlouli <elbahlouli.adil@etu.uae.ac.ma>
2025-10-26 13:43:11 +0100

    Update Exercice1.cpp

--- Atelier2/Atelier2/Exercice1.cpp
+++ Atelier2/Atelier2/Exercice1.cpp
@@ -1,27 +1,13 @@
 #include<iostream>
 using namespace std;
-
-bool estPair(int n) {
-    return n % 2 == 0;
+int n=1;
+void nbr_appell() {
+	cout<< "appel numero " << n << endl;
+	n++;
 }
-
-bool estMultipleDe3(int n) {
-    return n % 3 == 0;
-}
-
 int main() {
-    int nombre;
-    cout << "entrer un nombre entier : ";
-    cin >> nombre;
-    
-    if (estPair(nombre))
-        cout << "il est pair" << endl;
-    
-    if (estMultipleDe3(nombre))
-        cout << "il est multiple de 3" << endl;
-    
-    if (estPair(nombre) && estMultipleDe3(nombre))
-        cout << "il est divisible par 6" << endl;
-    
-    return 0;
+	nbr_appell();
+	nbr_appell();
+	nbr_appell();
+	return 0;
 }
